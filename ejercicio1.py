bd7a6dc52fb99665a8e8d9025600230c4a7c8877
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-07 18:23:03 +0100

    primera idea

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,11 @@ import random
 
 tabla = []
 
+tabla = [5, 7, 3, 2, 10, 9]
+for i in range (len(tabla)):
+    for j in range(len(tabla)):
+        if tabla[i] > tabla[j]:
+            aux = tabla[i]
+            tabla[i] = tabla[j]
+            tabla[j] = aux
+print(tabla)
