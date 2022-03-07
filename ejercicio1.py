5f0ff085f45cba88ec0429ff08a5d543c1afbdc9
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-07 18:19:39 +0100

    sigo luego

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,10 +2,4 @@
 import random
 
 tabla = []
-def generartabla(n, i):
-    j = i.split(" ")
-    while len(tabla) < n:
-        tabla.append(random.randint(int(j[0])), int(j[1]))
 
-generartabla(int(input("Introduzca cuántos elementos desea que tenga su tabla en un rango 1-50: ")))
-print("Su tabla inicial es la siguiente: " + str(tabla))
