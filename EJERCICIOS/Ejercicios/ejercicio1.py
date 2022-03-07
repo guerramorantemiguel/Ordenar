06aeeaf8426bb3132bb0450f75140653fb0fa777
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-07 17:32:12 +0100

    gen

--- EJERCICIOS/Ejercicios/ejercicio1.py
+++ EJERCICIOS/Ejercicios/ejercicio1.py
@@ -1,6 +1,11 @@
 import random
 
 tabla = []
+
+def generartabla(n, i):
+    j = i.split("-")
+    tabla.append(random.randint(int(j[0]), int(j[1])))
+    
 for i in range (len(tabla)):
     for j in range(len(tabla)):
         if tabla[i] > tabla[j]:
