cc6e84d3a09304419ae0bc42d838afed2b399032
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-07 18:05:30 +0100

    commits

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,4 +5,6 @@ tabla = []
 def generartabla(i, n):
     j = i.split(" ")
     while len(tabla) < i:
-        tabla.append(random.randint)
+        tabla.append(random.randint(int(j[0])), int(j[1]))
+
+generartabla(int(input("Introduzca cuántos elementos desea que tenga su tabla: ")))
